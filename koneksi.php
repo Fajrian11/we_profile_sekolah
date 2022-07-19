63c1751ec0880ca3d62532f229e69a987e9289ee
unknown <nugraha.fajrian@gmail.com>
2022-07-20 05:27:47 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+ 
+$databaseHost = 'localhost';
+$databaseName = 'wp_sekolah';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$koneksi = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+?>
+ 
